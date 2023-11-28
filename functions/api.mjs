712f24421a399575c49d6ebbd22cef9538c5465c
Botirjon007 <batyrzhanulmasov@gmail.com>
2023-11-29 04:13:25 +0500

    first uploading

---
+++ functions/api.mjs
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import ServerlessHttp from 'serverless-http';
+const app = express();
+
+const port = 9000;
+const data = [
+  { name: "User1", surname: "John" },
+  { name: "User1", surname: "Doe" },
+];
+
+app.use(bodyParser.json());
+app.get('/getAllUsers', (req, res) => {
+    res.json(data);
+});
+
+app.post('/addNewUser', (req, res) => {
+    data.push(req.body);
+    res.send('Successfully added');
+});
+
+/* app.listen(port, () => {
+    console.log('App is running on port', port);
+}); */
+
+export const handler =  ServerlessHttp(app);
